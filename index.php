495518dae26ec68646d7262aed39d1f490c5b0d1
JOAraka <69601408+JOAraka@users.noreply.github.com>
2020-09-04 14:47:06 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+  echo "<html>
+            <head><title><php added edit</title></head>
+            <body>
+            
+            <form>
+              <input type="text" name="names" method= "post" placeholder="what is your name"/>
+              <input type="submit" name= "submit" value= "submit"/>
+              
+            </form>
+            </body>
+         </html> "; 
+echo 
+  ?>
